d7f07f5321c411b42c8311c25dbf899ce7fc1cb4
Mishabaleila <lolixplora@ya.ru>
2024-05-08 13:57:54 +0300

    Add workflow with PEP8 linting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,4 @@
 import os
-
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -70,7 +69,7 @@ DATABASES = {
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
     }
-} 
+}
 
 
 # Password validation
